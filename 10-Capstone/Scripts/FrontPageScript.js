c0cf2ae4327e0bc373e8ec3e44e315c489f2179e
KyledeVos <kdevos827@gmail.com>
2022-08-10 12:52:46 +0200

    Added Feature HTML Links to new Feature Pages and Tested

--- 10-Capstone/Scripts/FrontPageScript.js
+++ 10-Capstone/Scripts/FrontPageScript.js
@@ -71,4 +71,59 @@ canvaLinkButtonFrontPage.addEventListener("mouseout", ()=>{
 // FEATURE OPTIONS - OTHER HTML PAGES IN DIRECTORY
 //###################################################################
 
+//1) New Product Link
+let newProductLink = document.getElementById("newProductLink");
+newProductLink.addEventListener("mouseover", ()=>{
+    newProductLink.setAttribute("style", 
+    "background-color: rgba(255,255,255, 0.2)");
+})
+
+newProductLink.addEventListener("mouseout", ()=>{
+    newProductLink.setAttribute("style", "background-color:transparent;");
+})
+
+//2)  Create Order Link
+let CreateOrderLink = document.getElementById("CreateOrderLink");
+CreateOrderLink.addEventListener("mouseover", ()=>{
+    CreateOrderLink.setAttribute("style", 
+    "background-color: rgba(255,255,255, 0.2)");
+})
+
+CreateOrderLink.addEventListener("mouseout", ()=>{
+    CreateOrderLink.setAttribute("style", "background-color:transparent;");
+})
+
+//3)  Current Products Link
+let currentProductsLink = document.getElementById("currentProductsLink");
+currentProductsLink.addEventListener("mouseover", ()=>{
+    currentProductsLink.setAttribute("style", 
+    "background-color: rgba(255,255,255, 0.2)");
+})
+
+currentProductsLink.addEventListener("mouseout", ()=>{
+    currentProductsLink.setAttribute("style", "background-color:transparent;");
+})
+
+//4)  Price Check Link
+let PriceCheckLink = document.getElementById("PriceCheckLink");
+PriceCheckLink.addEventListener("mouseover", ()=>{
+    PriceCheckLink.setAttribute("style", 
+    "background-color: rgba(255,255,255, 0.2)");
+})
+
+PriceCheckLink.addEventListener("mouseout", ()=>{
+    PriceCheckLink.setAttribute("style", "background-color:transparent;");
+})
+
+//4)  New Materials Link
+let newMaterialsLink = document.getElementById("newMaterialsLink");
+newMaterialsLink.addEventListener("mouseover", ()=>{
+    newMaterialsLink.setAttribute("style", 
+    "background-color: rgba(255,255,255, 0.2)");
+})
+
+newMaterialsLink.addEventListener("mouseout", ()=>{
+    newMaterialsLink.setAttribute("style", "background-color:transparent;");
+})
+
 
