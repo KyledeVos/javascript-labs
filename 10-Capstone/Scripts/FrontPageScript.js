d2cfacf92ca72c3b410a957c8d11193ed6aed065
KyledeVos <kdevos827@gmail.com>
2022-08-10 09:50:11 +0200

    Added Social Media Links. Created Basic Structure of other feature html pages

---
+++ 10-Capstone/Scripts/FrontPageScript.js
@@ -0,0 +1,74 @@
+//Script Controlling buttons on Front Page
+/*
+Tasks - Call Links at the top of the page
+      - Call other HTML Pages for features
+      - Control Search Bar
+*/
+
+//###################################################################
+// OTHER WEBSITE LINKS
+//###################################################################
+
+//Mouse Hover Actions - Change Color of Font
+
+//1) Instagram Link
+let instagramLinkButtonFrontPage = document.getElementById("instagramLinkButtonFrontPage");
+
+instagramLinkButtonFrontPage.addEventListener("mouseover", ()=>{
+    instagramLinkButtonFrontPage.setAttribute("style", "color:rgb(217, 216, 216)")
+});
+
+instagramLinkButtonFrontPage.addEventListener("mouseout", ()=>{
+    instagramLinkButtonFrontPage.setAttribute("style", "color:white")
+});
+
+
+//2) Facebook Link
+let facebookLinkButtonFrontPage = document.getElementById("facebookLinkButtonFrontPage");
+
+facebookLinkButtonFrontPage.addEventListener("mouseover", ()=>{
+    facebookLinkButtonFrontPage.setAttribute("style", "color:rgb(217, 216, 216)")
+});
+
+facebookLinkButtonFrontPage.addEventListener("mouseout", ()=>{
+    facebookLinkButtonFrontPage.setAttribute("style", "color:white")
+});
+
+//3) TikTok Link
+let tikTokLinkButtonFrontPage = document.getElementById("tikTokLinkButtonFrontPage");
+
+tikTokLinkButtonFrontPage.addEventListener("mouseover", ()=>{
+    tikTokLinkButtonFrontPage.setAttribute("style", "color:rgb(217, 216, 216)")
+});
+
+tikTokLinkButtonFrontPage.addEventListener("mouseout", ()=>{
+    tikTokLinkButtonFrontPage.setAttribute("style", "color:white")
+});
+
+//4) Gmail Link
+let gmailLinkButtonFrontPage = document.getElementById("gmailLinkButtonFrontPage");
+
+gmailLinkButtonFrontPage.addEventListener("mouseover", ()=>{
+    gmailLinkButtonFrontPage.setAttribute("style", "color:rgb(217, 216, 216)")
+});
+
+gmailLinkButtonFrontPage.addEventListener("mouseout", ()=>{
+    gmailLinkButtonFrontPage.setAttribute("style", "color:white")
+});
+
+//5) Canva Link
+let canvaLinkButtonFrontPage = document.getElementById("canvaLinkButtonFrontPage");
+
+canvaLinkButtonFrontPage.addEventListener("mouseover", ()=>{
+    canvaLinkButtonFrontPage.setAttribute("style", "color:rgb(217, 216, 216)")
+});
+
+canvaLinkButtonFrontPage.addEventListener("mouseout", ()=>{
+    canvaLinkButtonFrontPage.setAttribute("style", "color:white")
+});
+
+//###################################################################
+// FEATURE OPTIONS - OTHER HTML PAGES IN DIRECTORY
+//###################################################################
+
+
